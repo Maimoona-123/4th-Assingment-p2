f550d1d233448c7d1c6e145ec57acb79367190b5
maimoona <maimoonaaltaf7@gmail.com>
2024-09-19 07:17:20 -0700

    4th assingment

---
+++ app.js
@@ -0,0 +1,289 @@
+// chap -10      ( If statment )
+
+// Q-1
+
+//  var city = prompt ( "Enter city" );
+
+// if (city === "karachi") {
+//   console.log ("Welcome to city of lights..!");
+
+// }
+
+// else {
+//     console.log ( "Try again" )
+// }
+
+
+
+
+
+
+// Q-3
+
+// var city = prompt ( "Enter city" );
+
+// if ( city === "karachi") {
+//   alert ( "Please write a correct city ..." );
+
+// }
+
+// else {
+//     alert ( " Try again ..!" )
+// }
+
+
+
+
+
+// Q-4
+
+// var x = 5 ;
+
+// if ( x === 5 ) {
+//   x = 10
+//   console.log ( "x has been updated to " + x ) ;
+
+
+// } else {
+//   console.log ( "x is not equal to 5" )
+// }
+
+
+
+
+
+// Chap -11   ( Comparison operator )   
+
+
+
+// Q-2
+
+
+ 
+// var age = +prompt("Enter your age");
+
+// if (age >= 18) {
+//   console.log ("you are eligible for vote");
+
+
+// } else {
+//   console.log ("you are not eligible for vote");
+// }
+
+
+
+// Q-3
+
+
+// var price = prompt ( " Enter price " )
+
+// var price = 50 ;
+
+// if  ( price !== 75 ) {
+//   price = 75
+//   console.log ( "price updated to " + price  ) ;
+
+// } else {
+//   console.log ( "price is  already 75" )
+// }
+
+
+
+
+// Q-4
+
+// var num = prompt ( "Enter number" )
+
+// var x = 20 ;
+// var y = 30 ;
+
+// if ( x !== y ) {
+//   alert ( "Congragulation" )
+// }
+
+// else {
+//   alert ( "Try again" )
+// }
+
+
+
+
+// Q-5
+
+
+// var firstName = prompt (  " Enter firstname " );
+// var otherName = prompt ( "Enter othername " );
+
+// if ( firstName !== otherName ) {
+//   alert ( "No match" )
+
+// } else {
+//   alert ( "Try again" )
+// }
+
+
+
+
+
+// Chap - 12      ( if ...else and else if statment )
+
+
+
+// Q-2
+
+// var percentage = + prompt ( "Enter your percentage" )
+
+// if ( percentage >= 80 ) {
+//   document.write ( ` Total mark : 300  <br>
+//      Mark obtained : 250  <br>
+//      percentage : 90  <br>
+//       Grade : A-one   <br>
+//       Remark : Excellent ` ) 
+// }
+
+
+
+// if ( percentage >= 75 ) {
+//   document.write ( ` Total mark : 300  <br>
+//      Mark obtained : 200  <br>
+//      percentage : 70  <br>
+//       Grade : B   <br>
+//       Remark : Better need improvment ` ) 
+// }
+
+
+// if ( percentage >= 50 ) {
+//   document.write ( ` Total mark : 300  <br>
+//      Mark obtained : 140  <br>
+//      percentage : 50  <br>
+//       Grade : C   <br>
+//       Remark : Hard working  ` ) 
+// }
+
+
+
+
+
+// Q-4
+
+// var city = prompt ( "Enter a city" )
+
+// if ( city === "karachi" ) {
+//     alert ( "You have enter karachi" )
+
+// } else if ( city === "Lahore" ) {
+//     alert ( "You have enter lahore" )
+// }
+
+
+
+
+// chap - 13   ( Testing sets of conditions )
+
+
+// Q - 3
+
+// var name = "Hamza";
+// var age = 25 ;
+
+// if ( name === "Hamza" || name === "Arsalan" && age < 60 ) {
+//     console.log ( "Name is valid and age is less than 60 ..   " )
+// }
+
+
+
+
+
+// // Q - 4
+
+// prompt ( "Enter number 1" ) ;
+// prompt ( "Enter number 2" ) ;
+
+// var num1 = 10 ;
+// var num2 = 20 ;
+
+// if ( num1 < num2 ) {
+//     alert ( "num 1 is not equal to num 2 " )
+// }
+
+
+
+// Q-5 
+
+// var firstName = "John" ;
+// var lastName = "Doe" ;
+
+// var firstName = prompt ( "Enter your first name " ) ;
+// var lastName = prompt ( "Enter your last name " ) ;
+
+
+// if  (  firstName === firstName &&  lastName === lastName ) {
+//     alert  ("Welcome," + firstName + "" + lastName + "" )
+
+// } else {
+//     alert ( "Invalid username" )
+// }
+
+
+
+
+
+// Chapter 14   ( If statment nested )
+
+
+// Q-1
+
+
+
+
+// var password = prompt ( "Enter password" ) ;
+
+// if ( "password" ) {
+//     if ( password.length <= 5) {
+//         alert ( "password must be greater than 5" )
+    
+//     } else {
+//         alert ( "OK" )
+//     }
+// } else {
+//     alert ( "passwordcannot be empty" )
+// }
+
+
+
+
+// Q-2
+
+// var a = 1 ;
+// var c = "Max"
+
+// if ( a === 1 ) {
+//     if ( c === Max ) {
+//         alert ( "OK" )
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// Q-3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
